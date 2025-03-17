4dfbbb565757981c80e4deea6a919c0bc4c177a0
gawa <gwcs@cin.ufpe.br>
2025-03-17 03:34:09 -0300

    Localizar Contatos

--- src/main/java/com/gawa/contatos_app/service/ContatoService.java
+++ src/main/java/com/gawa/contatos_app/service/ContatoService.java
@@ -2,9 +2,14 @@ package com.gawa.contatos_app.service;
 
 import com.gawa.contatos_app.entity.Contato;
 import com.gawa.contatos_app.repository.ContatoRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
@@ -17,4 +22,31 @@ public class ContatoService {
         return contatoRepository.save(contato);
     }
 
+    @Transactional(readOnly = true)
+    public Contato buscarPorId(long contatoId) {
+        return contatoRepository.findById(contatoId).orElseThrow(
+                () -> new RuntimeException("Contato nao encontrado.")
+        );
+    }
+
+    @Transactional
+    public Contato atualizarParcialmente(Long contatoId, Map<String, Object> updates) {
+        Contato contato = contatoRepository.findById(contatoId)
+                .orElseThrow(() -> new RuntimeException("Contato não encontrado"));
+
+        updates.forEach((campo, valor) -> {
+            Field field = ReflectionUtils.findField(Contato.class, campo);
+            if (field != null) {
+                field.setAccessible(true);
+                ReflectionUtils.setField(field, contato, valor);
+            }
+        });
+
+        return contatoRepository.save(contato);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Contato> buscarTodos() {
+        return contatoRepository.findAll();
+    }
 }
--- src/main/java/com/gawa/contatos_app/web/controller/ContatoController.java
+++ src/main/java/com/gawa/contatos_app/web/controller/ContatoController.java
@@ -1,14 +1,12 @@
 package com.gawa.contatos_app.web.controller;
-
+import java.util.List;
+import java.util.Map;
+import org.springframework.web.bind.annotation.*;
 import com.gawa.contatos_app.entity.Contato;
 import com.gawa.contatos_app.service.ContatoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,4 +20,23 @@ public class ContatoController {
         Contato cont = contatoService.salvar(contato);
         return ResponseEntity.status(HttpStatus.CREATED).body(cont);
     }
+
+    @GetMapping("/{contato_id}")
+    public ResponseEntity<Contato> getById(@PathVariable long contato_id){
+        Contato cont = contatoService.buscarPorId(contato_id);
+        return ResponseEntity.ok(cont);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Contato>> getAllContatos(){
+        List<Contato> conts = contatoService.buscarTodos();
+        return ResponseEntity.ok(conts);
+    }
+
+    @PatchMapping("/{contato_id}")
+    public ResponseEntity<Contato> updateContato(@PathVariable Long contato_id, @RequestBody Map<String, Object> updates) {
+        Contato contatoAtualizado = contatoService.atualizarParcialmente(contato_id, updates);
+        return ResponseEntity.ok(contatoAtualizado);
+    }
+
 }
